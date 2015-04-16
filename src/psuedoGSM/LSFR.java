724a2b758f67632370acd8b3ac54adb5cbc963f3
Slyboots92 <michal.blach92@gmail.com>
2015-04-16 18:50:34 +0200

    init

--- src/psuedoGSM/LSFR.java
+++ src/psuedoGSM/LSFR.java
@@ -1,5 +1,23 @@
 package psuedoGSM;
 
+import java.util.ArrayList;
+
 public class LSFR {
 
+	private int [] array;
+	private ArrayList<Integer> poly;
+	public int[] getArray() {
+		return array;
+	}
+	public void setArray(int[] array) {
+		this.array = array;
+	}
+	public ArrayList<Integer> getPoly() {
+		return poly;
+	}
+	public void setPoly(ArrayList<Integer> poly) {
+		this.poly = poly;
+	}
+	
+	
 }
---
+++ src/psuedoGSM/Main.java
@@ -0,0 +1,11 @@
+package psuedoGSM;
+
+public class Main {
+
+	public static void main(String[] args) {
+			
+			LSFR r1 = new LSFR();
+			r1.setPoly();
+	}
+
+}
