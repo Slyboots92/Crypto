acd25849a916d391890da0394dd69b7adaf51e13
Slyboots92 <michal.blach92@gmail.com>
2015-04-29 19:02:50 +0200

    fixed

--- src/lcg/BrutalForceThread.java
+++ src/lcg/BrutalForceThread.java
@@ -1,43 +1,112 @@
 package lcg;
 
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class BrutalForceThread extends Thread{
 	
-	private Long m;
-	private boolean flag;
 	private Long sn;
-	private CopyOnWriteArrayList<Pair> result;
-	private int number;
 	private Long sn1;
-	public boolean isFlag() {
-		return flag;
-	}
-
-	public void setFlag(boolean flag) {
-		this.flag = flag;
-	}
-	public BrutalForceThread(int number , Long m,Long sn,Long sn1,
-			CopyOnWriteArrayList<Pair>result){
-		this.m=m;
+	private Long sn2;
+	private Long sn3;
+	private Long m;
+	private Long beg;
+	private Long end;
+	private int threadId;
+	public BrutalForceThread(int threadId,Long sn,Long sn1,Long sn2,Long sn3,Long m, Long beg, Long end){
 		this.sn=sn;
 		this.sn1=sn1;
-		this.result=result;
-		this.flag=true;
-		this.number=number;
+		this.sn2=sn2;
+		this.sn3=sn3;
+		this.m=m;
+		this.beg=beg;
+		this.end=end;
+		this.threadId=threadId;
 	}
 	
 	@Override
 	public void run() {
-		for(Long i=0L;i<m;i++){
-			System.out.println("number "+number+" a "+i);
-			for(Long j=0L;j<m;j++){
-				if(sn1==((i*sn)+j)%m){
-					result.add(new Pair(i, j));
+		
+		for(int i=0;i<=m.intValue();i++){
+			for(int j=0;j<=m.intValue();j++){
+				
+				if(sn1.intValue()==((i*sn.intValue()+j)%m)){
+					if(checkIsThisPairIsCorrect(i,j,sn3,sn2,sn1)){
+						System.err.println("Rozwiazanie a "+i+" b "+j);
+					}
 				}
 			}
 		}
-		flag=false;
 	}
-	
+	private boolean  checkIsThisPairIsCorrect(int a,int b,Long sn3,Long sn2,Long sn1){
+
+		if(sn3.equals((a*sn2+b)%m)&&sn2.equals((sn1*a+b)%m)){
+			
+			return true;
+		}
+		else
+			return false;
+	}
+	public Long getSn() {
+		return sn;
+	}
+
+	public void setSn(Long sn) {
+		this.sn = sn;
+	}
+
+	public Long getSn1() {
+		return sn1;
+	}
+
+	public void setSn1(Long sn1) {
+		this.sn1 = sn1;
+	}
+
+	public Long getSn2() {
+		return sn2;
+	}
+
+	public void setSn2(Long sn2) {
+		this.sn2 = sn2;
+	}
+
+	public Long getSn3() {
+		return sn3;
+	}
+
+	public void setSn3(Long sn3) {
+		this.sn3 = sn3;
+	}
+
+	public Long getM() {
+		return m;
+	}
+
+	public void setM(Long m) {
+		this.m = m;
+	}
+
+	public Long getBeg() {
+		return beg;
+	}
+
+	public void setBeg(Long beg) {
+		this.beg = beg;
+	}
+
+	public Long getEnd() {
+		return end;
+	}
+
+	public void setEnd(Long end) {
+		this.end = end;
+	}
+
+	public int getThreadId() {
+		return threadId;
+	}
+
+	public void setThreadId(int threadId) {
+		this.threadId = threadId;
+	}
+
 }
